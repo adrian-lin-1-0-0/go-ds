19309eff74df7ce10df868895d921f9ae4d59c60
Adrian Lin <adrian.lin.1.0.0@gmail.com>
2024-03-05 14:21:00 +0800

    feat :using slice instead of struct type

--- tree/bit.go
+++ tree/bit.go
@@ -2,35 +2,29 @@ package tree
 
 import "golang.org/x/exp/constraints"
 
-type BIT[integer constraints.Integer] struct {
-	n   int
-	bit []integer
+type BIT[integer constraints.Integer] []integer
+
+func NewBIT[integer constraints.Integer](n int) BIT[integer] {
+	return make([]integer, n+1)
 }
 
-func NewBIT[integer constraints.Integer](n int) *BIT[integer] {
-	return &BIT[integer]{
-		n:   n,
-		bit: make([]integer, n+1),
-	}
-}
-
-func (b *BIT[integer]) Update(idx int, diff integer) {
-	for idx <= b.n {
-		b.bit[idx] += diff
+func (b BIT[integer]) Update(idx int, diff integer) {
+	for idx <= len(b)-1 {
+		b[idx] += diff
 		idx += lowbit(idx)
 	}
 }
 
-func (b *BIT[integer]) Query(idx int) integer {
+func (b BIT[integer]) Query(idx int) integer {
 	var res integer
 	for idx > 0 {
-		res += b.bit[idx]
+		res += b[idx]
 		idx -= lowbit(idx)
 	}
 	return res
 }
 
-func (b *BIT[integer]) RangeQuery(l, r int) integer {
+func (b BIT[integer]) RangeQuery(l, r int) integer {
 	return b.Query(r) - b.Query(l-1)
 }
 
